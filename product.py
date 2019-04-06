442bc2e1714bae7645f71fd537922af7257d16a6
SandyHuang0305 <42807293+SandyHuang0305@users.noreply.github.com>
2019-04-06 20:49:14 +0700

    add some stuff

--- product.py
+++ product.py
@@ -1,26 +1,37 @@
-#記帳程式
-products = [] #建立大清單
-with open('products.csv', 'r',ecoding='utf-8') as f: #讀取檔案#寫入與讀取檔案的編碼需相同
-    for line in f: #line為變數
-    	s = line.strip().split(',')#1.去掉換行符號2.切割,括號內輸入遇到甚麼時要切割
-    	print(s)
+import os #operating system作業系統
 
+products = [] #建立大清單
+#檢查檔案存在與否
+if os.path.isfile('products.csv'):#如果，路徑中有這個檔案
+    print('找到檔案')
+    #讀取檔案
+    with open('products.csv', 'r', encoding='utf-8') as f:#讀取檔案#寫入與讀取檔案的編碼需相同
+        for line in f: #line為變數
+            if '商品,價格' in line: #跳過商品價格字串
+                continue #不會跳出迴圈,只是前往下一迴，只能寫在迴圈裡
+            name, price = line.strip().split(',')#1.去掉換行符號2.切割,括號內遇到甚麼時就切割
+            products.append([name, price]) #建立小清單更簡易的方法
+    print(products)
+else:
+    print('找不到檔案')    
+   
+#讓使用者輸入    	
 while True:
     name = input('請輸入商品名稱:')
     if name == 'q':
     	break
     price = input('請輸入商品價格:')	
-    p = [] #建立小清單
-    p = [name, price]
-    products.append(p)#把p裝進products
+    price = int(price)
+    products.append([name, price])#把p裝進products
 print(products)
 
-products[1][1]#大清單的第一個中的第一格    	
+#印出購買紀錄    	
+for p in products: #for loop 教學
+	print(p[0], '的價格為', p[1])
 
-for product in products: #for loop 教學
-	print(product[0], '的價格為', product[1])
-with open ('products.csv', 'w', ecoding='utf-8')	as f: #依序為1.檔名2.寫入模式3.寫入當作f 4.處理字體亂碼問題
+#寫入檔案
+with open ('products.csv', 'w', encoding='utf-8')	as f: #依序為1.檔名2.寫入模式3.寫入當作f 4.處理字體亂碼問題
     f.write('商品,價格\n')
     for p in products:
-    	f.write(p[0] +  ',' +  p[1] + '\n') #真正的寫入檔案
+    	f.write(p[0] +  ',' +  str(p[1]) + '\n') #真正的寫入檔案
     	                                    #csv檔以逗點做區格
