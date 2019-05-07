9027c6574b40d09a9b8b258abebc2a864e8130c5
SandyHuang0305 <42807293+SandyHuang0305@users.noreply.github.com>
2019-05-07 21:34:14 +0700

    增加路徑合併功能

--- picture.py
+++ picture.py
@@ -2,7 +2,8 @@ from PIL import Image #使用PIL套件中的Image功能
 import os
 
 #一次轉換多張圖片
-for file in os.listdir('.'): #在這個文件夾之下
+for file in os.listdir('orig'): #在這個文件夾之下 #增加路徑合併
     if file.endswith('.jpg'): #不要印出python檔的處理方式
-        img = Image.open(file).convert('L')
-        img.save(file[:-4] + '_grey.png')#存檔
+        img = Image.open('orig/' + file).convert('L')
+        img.save('result/' + file[:-4] + '_grey.png')#存檔
+#路徑合併的其他方式: (os.path,join('orig', file))
