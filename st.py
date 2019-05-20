9091b6e6675c2f9e576b3f0dfb0bb45aee4c0ac3
SandyHuang0305 <42807293+SandyHuang0305@users.noreply.github.com>
2019-05-20 22:46:29 +0700

    修改

--- st.py
+++ st.py
@@ -1,19 +1,23 @@
 #class 類別
 class Student:
-    def __init__(self, name):#初始化
+    def __init__(self, name, score):#初始化
         self.name = name
-        self.do_hw()
-        self.study()
-        self.sleep()
+        self.score = score
         print('我誕生了')
 
-    def do_hw(self):
-        print('我在做作業~')
+    def do_hw(self, hw):
+        print('我在做作業~', hw)
 
     def study(self):
         print('我在讀書...')
+        self.score += 5
 
     def sleep(self):
         print('終於能睡覺了...')
-s = Student('zero')
-print(s.name)    
+s1 = Student('zero', 100)
+s2 = Student('sky', 95)
+print(s1.name, s1.score) 
+print(s2.name, s2.score)
+
+s2.study()
+print(s2.name, s2.score)  
