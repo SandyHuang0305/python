326923e6b83436afd8e74b1db866c1f796f393f0
SandyHuang0305 <42807293+SandyHuang0305@users.noreply.github.com>
2019-06-10 23:01:17 +0700

    import 練習

--- import_test.py
+++ import_test.py
@@ -20,3 +20,19 @@ print(today.strftime('%Y/%m/%d'))
 print(today.strftime('%Y{y}%m{m}%d{d}'). format(y= '年', m = '月', d = '日'))
 
 print(today.strftime('%Y %B %d %a'))
+
+#現在時間
+from datetime import datetime as dt
+print(dt.now())
+
+now = dt.now()
+print(now.strftime('%Y-%m-%d %H:%M:%S'))
+
+#timedelta
+from datetime import date, timedelta
+today = date.today()
+print(today)
+
+one_week = timedelta(days = 7)
+print(today + one_week)
+print(today - one_week)
