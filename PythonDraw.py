39a6c19fb6f4ec3e08839c453bed7d636d046913
SandyHuang0305 <42807293+SandyHuang0305@users.noreply.github.com>
2019-06-15 21:47:15 +0700

    增加註解 + 改顏色

--- PythonDraw.py
+++ PythonDraw.py
@@ -1,14 +1,15 @@
 #PythonDraw.py
+#以絕對方向與絕對角度定義
 import turtle
-turtle.setup(650, 350, 200, 200)
+turtle.setup(650, 350, 200, 200) #定義窗體大小#非必須設置
 turtle.penup()
-turtle.fd(-250)
+turtle.fd(-250) #向前運行
 turtle.pendown()
 turtle.pensize(25)
-turtle.pencolor('purple')
-turtle.seth(-40)
+turtle.pencolor('cyan')
+turtle.seth(-40) #改變行進角度senth(angle)
 for i in range(4):
-    turtle.circle(40, 80)
+    turtle.circle(40, 80) #曲線運行
     turtle.circle(-40, 80)
 turtle.circle(40, 80/2)
 turtle.fd(40)
