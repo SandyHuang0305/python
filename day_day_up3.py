23f9389ec1cc558277f70453eb8e813297e89c54
SandyHuang0305 <42807293+SandyHuang0305@users.noreply.github.com>
2019-06-23 15:29:43 +0700

    天天向上練習三--有休息日的情況

--- day_day_up3.py
+++ day_day_up3.py
@@ -1,2 +1,11 @@
 # 天天向上第三練習-有休息日的情況
 dayup = 1.0
+dayfactor = 0.01
+for i in range (365):
+    #有假日的情況-休息
+    if i % 7 in [6, 0]:
+        dayup = dayup * (1 - dayfactor)
+    #工作日
+    else:
+        dayup = dayup * (1 + dayfactor)
+print('工作日的力量: {:.2f}'.format(dayup))
