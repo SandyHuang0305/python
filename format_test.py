de6fe56dd2feca9d966e961b4cb10f9cd22c63b8
SandyHuang0305 <42807293+SandyHuang0305@users.noreply.github.com>
2019-07-02 22:33:36 +0700

    字串符相關練習

---
+++ format_test.py
@@ -0,0 +1,6 @@
+#字串符相關練習
+print('{0:=^20}'.format('python'))
+print('{0:*>20}'.format('BIT'))
+print('{:10}'.format('BIT'))
+print('{0:b}, {0:c}, {0:d}, {0:o}, {0:x}'.format(425))
+print('{0:,.2f}'.format(12345.6789))
