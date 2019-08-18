2c77a102a044bf786b3879ead995aff2893921bc
SandyHuang0305 <42807293+SandyHuang0305@users.noreply.github.com>
2019-08-18 18:35:44 +0700

    中文詞頻統計(jieba)

---
+++ jieba_word_count_cn.py
@@ -0,0 +1,15 @@
+#中文詞頻統計
+import jieba
+txt = open("WuMing1.txt", 'r', encoding='utf-8').read() #讀檔
+words = jieba.lcut(txt) #切割文檔的文字
+counts = {} #設置字典
+for word in words: #遍例文檔中所有文字並計數
+    if len(word) == 1:
+        continue
+    else:
+        counts[word] = counts.get(word, 0) + 1
+items = list(counts.items()) #建立列表
+items.sort(key=lambda x:x[1], reverse=True)
+for i in range(15): #抓前15位
+    word, count = items[i]
+    print('{0:<10}{1:>5}'.format(word, count))
