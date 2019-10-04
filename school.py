06a7a13c0373e129c72ea16786f04b727b054df8
SandyHuang0305 <42807293+SandyHuang0305@users.noreply.github.com>
2019-10-04 18:01:42 +0700

    第二版
    更新對齊方式

--- school.py
+++ school.py
@@ -1,4 +1,4 @@
-#中國大學排行
+#大學排行
 import requests
 from bs4 import BeautifulSoup
 
@@ -20,10 +20,11 @@ def fillUnivList(ulist, html):#將頁面放到列表
             ulist.append([tds[0].string, tds[1].string, tds[3].string])   
 
 def printUnivList(ulist, num): #打出列表並設定要輸出幾所學校
-    print("{:^10}\t{:^6}\t{:^10}".format('排名', '學校名稱', '總分'))
+    tplt = "{0:^10}\t{1:{3}^10}\t{2:^10}"
+    print(tplt.format('排名', '學校名稱', '總分',chr(12288)))
     for i in range(num): #要輸出的數量
         u = ulist[i]
-        print('{:^10}\t{:^6}\t{:^10}'.format(u[0], u[1], u[2]))
+        print(tplt.format(u[0], u[1], u[2],chr(12288)))
 def main():
     uinfo = []
     url = 'http://www.zuihaodaxue.com/zuihaodaxuepaiming2019.html'
